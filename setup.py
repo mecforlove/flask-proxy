9715cf0fd59ef413163229f54f383f9816bf5cf4
mecforlove <mecforlove@outlook.com>
2018-05-17 19:30:24 +0800

    Format setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,12 @@ import re
 from os import path
 from setuptools import setup, find_packages
 
-requirments = [
-    'Flask>=0.1',
-    'requests>=2.0'
-]
+requirments = ['Flask>=0.1', 'requests>=2.0']
 
 version_file = path.join(
-    path.dirname(__file__),
-    'flask_proxy',
-    '__version__.py'
-)
+    path.dirname(__file__), 'flask_proxy', '__version__.py')
 with open(version_file, 'r') as fp:
-    m = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]",
-        fp.read(),
-        re.M
-    )
+    m = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", fp.read(), re.M)
     version = m.groups(1)[0]
 
 setup(
@@ -28,11 +18,10 @@ setup(
     author_email='mecforlove@outlook.com',
     description='',
     packages=find_packages(exclude=['tests']),
-        classifiers=[
+    classifiers=[
         'Framework :: Flask',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: BSD License',
     ],
-    install_requires=requirments
-)
+    install_requires=requirments)
